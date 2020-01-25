63ccaca58947a440e782a48f0701e45b4e42cd4c
blisstik <42245908+blisstik@users.noreply.github.com>
2020-01-24 18:47:12 -0800

    lightrec

--- scriptmodules/libretrocores/lr-beetle-psx.sh
+++ scriptmodules/libretrocores/lr-beetle-psx.sh
@@ -9,45 +9,44 @@
 # at https://raw.githubusercontent.com/Retro-Arena/RetroArena-Setup/master/LICENSE.md
 ##
 
-rp_module_id="lr-beetle-psx-hw"
+rp_module_id="lr-beetle-psx"
 rp_module_desc="PlayStation emulator - Mednafen PSX Port for libretro with lightrec"
 rp_module_help="ROM Extensions: .bin .cue .cbn .img .iso .m3u .mdf .pbp .toc .z .znx\n\nCopy your PlayStation roms to $romdir/psx\n\nCopy the required BIOS files\n\nscph5500.bin and\nscph5501.bin and\nscph5502.bin to\n\n$biosdir"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/beetle-psx-libretro/master/COPYING"
 rp_module_section="lr"
 rp_module_flags=""
 
-function depends_lr-beetle-psx-hw() {
+function depends_lr-beetle-psx() {
     local depends=(libgl1-mesa-dev)
     getDepends "${depends[@]}"
 }
 
-function sources_lr-beetle-psx-hw() {
+function sources_lr-beetle-psx() {
     gitPullOrClone "$md_build" https://github.com/libretro/beetle-psx-libretro.git
 }
 
-function build_lr-beetle-psx-hw() {
+function build_lr-beetle-psx() {
     make clean
-    #make HAVE_HW=1
     make HAVE_LIGHTREC=1
     md_ret_require=(
         'mednafen_psx_hw_libretro.so'
     )
 }
 
-function install_lr-beetle-psx-hw() {
+function install_lr-beetle-psx() {
     md_ret_files=(
         'mednafen_psx_hw_libretro.so'
     )
 }
 
-function install_bin_lr-beetle-psx-hw() {
-    downloadAndExtract "$__gitbins_url/lr-beetle-psx-hw.tar.gz" "$md_inst" 1
+function install_bin_lr-beetle-psx() {
+    downloadAndExtract "$__gitbins_url/lr-beetle-psx.tar.gz" "$md_inst" 1
 }
 
-function configure_lr-beetle-psx-hw() {
+function configure_lr-beetle-psx() {
     mkRomDir "psx"
     ensureSystemretroconfig "psx"
 
-    addEmulator 0 "$md_id" "psx" "$md_inst/mednafen_psx_hw_libretro.so"
+    addEmulator 0 "$md_id" "psx" "$md_inst/mednafen_psx_libretro.so"
     addSystem "psx"
 }
